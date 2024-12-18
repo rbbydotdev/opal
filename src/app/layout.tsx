22f372a9bfcc5abd0151d6690946a57af9b4e74c
Robby P <101137670+rbbydotdev@@users.noreply.github.com>
2024-12-18 07:46:58 +0700

    basic layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* <SidebarProvider>{children}</SidebarProvider> */}
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,34 @@
+import { Navbar } from "@/app/Navbar";
+import { AppSidebar } from "@/components/app-sidebar";
 import { Editor } from "@/components/Editor/Editor";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import fs from "fs";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="w-screen h-screen border-4 border-black">{children}</div>;
-};
 export default function Home() {
+  const md = fs.readFileSync(process.cwd() + "/src/app/kitchen-sink.md", "utf-8");
   return (
-    <Layout>
-      <Editor markdown={"# Hello World\n\n## Hello World\n\n### Hello World\n\nHello World"} />
-    </Layout>
+    <SidebarProvider>
+      <div className="flex w-full h-screen">
+        <div className="">
+          <AppSidebar />
+        </div>
+        <div className="flex-grow h-screen flex">
+          <div className="w-full flex flex-col">
+            <Navbar>
+              <SidebarTrigger />
+            </Navbar>
+            <div className=" flex-grow">
+              <div className="overflow-auto min-w-full w-0 ">
+                <Editor
+                  markdown={md}
+                  className="flex flex-col"
+                  contentEditableClassName="max-w-full overflow-auto content-editable"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </SidebarProvider>
   );
 }
