7683a9a4d8255acbb8da4a9a7c134b0ae1aa2368
Robby P <101137670+rbbydotdev@@users.noreply.github.com>
2024-12-18 07:46:58 +0700

    Navbar component and implement Sidebar functionality

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,26 @@
+import { ButtonLink } from "@/components/ui/button";
+import clsx from "clsx";
+import { Children } from "react";
+
+export function Navbar({ className, children }: { children?: React.ReactNode; className?: string }) {
+  return (
+    <nav className={clsx("w-full flex", "min-h-[--navbar-height]", className)}>
+      <ul className="flex gap-2 justify-center items-center">
+        {Children.map(children, (child, index) => (
+          <li key={index} className="flex">
+            {child}
+          </li>
+        ))}
+        <li className="flex">
+          <ButtonLink variant={"outline"}>Three</ButtonLink>
+        </li>
+        <li className="flex">
+          <ButtonLink variant={"outline"}>Three</ButtonLink>
+        </li>
+        <li className="flex">
+          <ButtonLink variant={"outline"}>Three</ButtonLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- src/components/Editor/InitializedMDXEditor.tsx
+++ src/components/Editor/InitializedMDXEditor.tsx
@@ -1,36 +1,16 @@
 "use client";
+import { AllPlugins } from "@/components/Editor/AllPlugins";
 // InitializedMDXEditor.tsx
-import {
-  headingsPlugin,
-  listsPlugin,
-  markdownShortcutPlugin,
-  MDXEditor,
-  quotePlugin,
-  thematicBreakPlugin,
-  type MDXEditorMethods,
-  type MDXEditorProps,
-} from "@mdxeditor/editor";
+import { MDXEditor, type MDXEditorMethods, type MDXEditorProps } from "@mdxeditor/editor";
 import "@mdxeditor/editor/style.css";
 import type { ForwardedRef } from "react";
+// import {
+// } from '../'
 
 // Only import this to the next file
 export default function InitializedMDXEditor({
   editorRef,
   ...props
 }: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) {
-  return (
-    <MDXEditor
-      plugins={[
-        // Example Plugin Usage
-        headingsPlugin(),
-        listsPlugin(),
-        quotePlugin(),
-        thematicBreakPlugin(),
-        markdownShortcutPlugin(),
-      ]}
-      {...props}
-      ref={editorRef}
-      contentEditableClassName="prose"
-    />
-  );
+  return <MDXEditor plugins={AllPlugins} {...props} ref={editorRef} />;
 }
