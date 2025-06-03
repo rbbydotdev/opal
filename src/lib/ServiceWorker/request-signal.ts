01f0709db497d8cbe5027d58f48110cb519caf3e
rbbydotdev <robby.polana+github@gmail.com>
2025-06-03 22:27:08 +0700

    ok

--- src/lib/ServiceWorker/request-signal.ts
+++ src/lib/ServiceWorker/request-signal.ts
@@ -1,9 +1,13 @@
-const START = "request-start" as const;
-const END = "request-end" as const;
+const START = "REQUEST_START" as const;
+const END = "REQUEST_END" as const;
+const COUNT = "REQUEST_COUNT" as const;
 
 export const REQ_SIGNAL = {
   START,
   END,
+  COUNT,
 };
+
+export const REQ_NAME = "REQUEST_SIGNAL" as const;
 
 export type ReqSignal = (typeof REQ_SIGNAL)[keyof typeof REQ_SIGNAL];
