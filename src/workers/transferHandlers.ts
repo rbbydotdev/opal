f9cf80745fe59c5ab0c49e1e64b487545e03250a
rbbydotdev <robby.polana+github@gmail.com>
2025-06-08 20:45:29 +0700

    FUCK YA

--- src/workers/transferHandlers.ts
+++ src/workers/transferHandlers.ts
@@ -6,6 +6,7 @@ transferHandlers.set("asyncGenerator", asyncGeneratorTransferHandler);
 transferHandlers.set("Workspace", {
   canHandle: (obj): obj is Workspace => obj instanceof Workspace,
   serialize: (obj: Workspace) => {
+    console.log(obj.toJSON());
     return [
       { value: obj.toJSON() }, // Only serializable data
       [], // No transferable objects
