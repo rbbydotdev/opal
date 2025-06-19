667f15c49abd65c4f4e20216c01ac6120fa47dca
rbbydotdev <robby.polana+github@gmail.com>
2025-06-19 10:58:43 +0700

    focus highlighting for search results in the editor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,7 +55,10 @@ export default {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        highlight: "hsl(var(--highlight))",
+        highlight: {
+          DEFAULT: "hsl(var(--highlight))",
+          focus: "hsl(var(--highlight-focus))",
+        },
         chart: {
           "1": "hsl(var(--chart-1))",
           "2": "hsl(var(--chart-2))",
