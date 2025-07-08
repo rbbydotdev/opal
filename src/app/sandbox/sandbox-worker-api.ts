ecbac7f8498c5318b0c43cb90e92e819e513564a
rbbydotdev <robby.polana+github@gmail.com>
2025-07-09 03:26:28 +0700

    refactor: remove unused sandbox worker code and related files

--- src/app/sandbox/sandbox-worker-api.ts
+++
@@ -1,8 +0,0 @@
-export const SandboxWorkerAPI = {
-  async dothing() {
-    const randMs = Math.floor(Math.random() * 10) * 1000; // 1 to 10 seconds
-    await new Promise((rs) => setTimeout(rs, randMs));
-    return randMs;
-  },
-};
-export type SandboxWorkerAPIType = typeof SandboxWorkerAPI;
--- src/app/sandbox/sandbox.ww.ts
+++
@@ -1,4 +0,0 @@
-import { SandboxWorkerAPI } from "@/app/sandbox/sandbox-worker-api";
-import { expose } from "comlink";
-
-export const SandboxWorker = expose(SandboxWorkerAPI);
