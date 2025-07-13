f16078e83fa9007548207778356f56af7067a5cd
rbbydotdev <robby.polana+github@gmail.com>
2025-07-13 08:39:44 +0700

    replaceImageUrlsInMarkdown function for batch image URL replacement in markdown files

---
+++ src/lib/markdown/replaceImageUrlsInMarkdown.ts
@@ -0,0 +1,50 @@
+import { decodePath, encodePath } from "@/lib/paths2";
+import mdast from "mdast";
+import remarkDirective from "remark-directive";
+import remarkGfm from "remark-gfm";
+import remarkParse from "remark-parse";
+import remarkStringify from "remark-stringify";
+import { unified } from "unified";
+import { visit } from "unist-util-visit";
+export function replaceImageUrlsInMarkdown(
+  markdown: string,
+  findReplace: [fromUrl: string, toUrl: string][],
+  _origin = ""
+): [content: string, changed: boolean] {
+  const processor = unified().use(remarkParse).use(remarkGfm).use(remarkDirective);
+
+  // Parse the markdown into an AST
+  const tree = processor.parse(markdown);
+  let changed = false;
+
+  // Visit all image nodes and replace URLs as needed
+  visit(tree, "image", (node: mdast.Image) => {
+    for (const [fromUrl, toUrl] of findReplace) {
+      const decodedFromUrl = decodePath(fromUrl);
+      const decodedToUrl = decodePath(toUrl);
+      let pathname = decodePath(node.url);
+      if (pathname.startsWith(origin) && (pathname.startsWith("http://") || pathname.startsWith("https://"))) {
+        try {
+          const url = new URL(pathname);
+          pathname = url.pathname;
+        } catch (_e) {}
+      }
+      if (pathname === decodedFromUrl) {
+        node.url = encodePath(decodedToUrl);
+        changed = true;
+        break;
+      }
+    }
+  });
+
+  // Stringify the AST back to markdown
+  const processed = unified()
+    .use(remarkStringify, {
+      bullet: "-",
+      fences: true,
+      listItemIndent: "one",
+    })
+    .stringify(tree);
+
+  return [processed, changed];
+}
