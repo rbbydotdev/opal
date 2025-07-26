9ef7e28e550fd987030533db8472a059a2fb4dd7
rbbydotdev <robby.polana+github@gmail.com>
2025-07-26 17:56:24 +0700

    ok

--- src/features/git-repo/GitRepo.ts
+++ src/features/git-repo/GitRepo.ts
@@ -1,113 +1,90 @@
 import { CommonFileSystem } from "@/Db/CommonFileSystem";
-import { AbsPath } from "@/lib/paths2";
+import { absPath, AbsPath } from "@/lib/paths2";
 import git, { AuthCallback } from "isomorphic-git";
 import http from "isomorphic-git/http/web";
-export class GitRepo {
-  private fs: CommonFileSystem;
-  private dir: string;
 
-  constructor(fs: CommonFileSystem, dir: string) {
-    this.fs = fs;
-    this.dir = dir;
-  }
-
-  async clone(url: string, options?: Parameters<typeof git.clone>[0]) {
-    return git.clone({
-      fs: this.fs,
-      dir: this.dir,
-      http,
-      url,
-      ...options,
-    });
-  }
-
-  async status(filepath: string) {
-    return git.status({
-      fs: this.fs,
-      dir: this.dir,
-      filepath,
-    });
-  }
-
-  async add(filepath: string) {
-    return git.add({
-      fs: this.fs,
-      dir: this.dir,
-      filepath,
-    });
-  }
-
-  async commit(message: string, author: { name: string; email: string }) {
-    return git.commit({
-      fs: this.fs,
-      dir: this.dir,
-      message,
-      author,
-    });
-  }
-
-  async log(options?: Partial<Parameters<typeof git.log>[0]>) {
-    return git.log({
-      fs: this.fs,
-      dir: this.dir,
-      ...options,
-    });
-  }
-
-  // Add more methods as needed...
+interface Remote {
+  branch: string;
+  name: string;
+  url: string;
+  auth?: AuthCallback;
 }
 
-class Repo {
+export class Repo {
   fs: CommonFileSystem;
   dir: AbsPath;
   branch: string;
-  url: string;
-  remote: string;
 
-  auth: AuthCallback;
-
-  static New() {
-    return new Repo({});
+  New(fs: CommonFileSystem, dir: AbsPath = absPath("/"), branch: string = "main"): Repo {
+    return new Repo({ fs, dir, branch });
   }
-  constructor({
-    fs,
-    dir,
-    branch,
-    url,
-    remote,
-    auth,
-  }: {
-    fs: CommonFileSystem;
-    dir: AbsPath;
-    branch: string;
-    url: string;
-    remote: string;
-    auth: AuthCallback;
-  }) {
+  constructor({ fs, dir, branch }: { fs: CommonFileSystem; dir: AbsPath; branch: string }) {
     this.fs = fs;
-    this.auth = auth;
     this.dir = dir;
     this.branch = branch;
-    this.url = url;
+  }
+  withRemote(remote: Remote): RepoWithRemote {
+    return new RepoWithRemote(
+      {
+        fs: this.fs,
+        dir: this.dir,
+        branch: this.branch,
+        remoteBranch: remote.branch,
+        remoteName: remote.name,
+        url: remote.url,
+        auth: remote.auth,
+      },
+      remote
+    );
+  }
+}
+export class RepoWithRemote extends Repo {
+  readonly remote: Remote;
+
+  constructor(
+    {
+      fs,
+      dir,
+      branch,
+    }: {
+      fs: CommonFileSystem;
+      dir: AbsPath;
+      branch: string;
+      remoteBranch?: string;
+      remoteName: string;
+      url: string;
+      auth?: AuthCallback;
+    },
+    remote: Remote
+  ) {
+    super({ fs, dir, branch });
     this.remote = remote;
   }
 }
-
-export class GitPlaybook {
+class Playbook {
   constructor(private repo: Repo) {}
+  async addRemote(remote: Remote) {
+    await git.addRemote({
+      fs: this.repo.fs,
+      dir: this.repo.dir,
+      remote: remote.name,
+      url: remote.url,
+      force: true,
+    });
+    return this.repo.withRemote(remote);
+  }
+}
 
-  async initRepo(repo?: Repo) {
+class RemotePlaybook extends Playbook {
+  constructor(private repo: RepoWithRemote) {
+    super(repo);
+  }
+
+  async initRepo() {
     // Initialize a new git repository
     await git.init({
       fs: this.repo.fs,
       dir: this.repo.dir,
-    });
-    await git.addRemote({
-      fs: this.repo.fs,
-      dir: this.repo.dir,
-      remote: this.repo.remote,
-      url: this.repo.url,
-      force: true,
     });
   }
   async push() {
@@ -116,20 +93,27 @@ export class GitPlaybook {
       fs: this.repo.fs,
       http,
       dir: this.repo.dir,
-      remote: this.repo.remote,
+      remote: this.repo.remote.name,
       ref: this.repo.branch, // or any branch you want to push to
-      onAuth: this.repo.auth,
+      onAuth: this.repo.remote.auth,
     });
   }
-  async pull({ remoteBranch = "main" }: { remoteBranch?: string } = {}) {
+  async pull() {
     /*fetch,merge*/
-    await git.pull({
+    await git.fetch({
       fs: this.repo.fs,
       http,
       dir: this.repo.dir,
-      remote: this.repo.remote,
-      ref: remoteBranch,
-      onAuth: this.repo.auth,
+      remote: this.repo.remote.name,
+      ref: this.repo.remote.branch, // or any branch you want to fetch from
+      onAuth: this.repo.remote.auth,
+    });
+    await git.merge({
+      fs: this.repo.fs,
+      dir: this.repo.dir,
+      ours: this.repo.branch, // or any branch you want to merge into
+      theirs: this.repo.remote.branch, // or any branch you want to merge from
+      fastForwardOnly: true, // Set to false if you want to allow non-fast-forward merges
     });
   }
   async syncWithRemote() {
@@ -142,3 +126,58 @@ export class GitPlaybook {
       });
   }
 }
+
+// export class GitRepo {
+//   private fs: CommonFileSystem;
+//   private dir: string;
+
+//   constructor(fs: CommonFileSystem, dir: string) {
+//     this.fs = fs;
+//     this.dir = dir;
+//   }
+
+//   async clone(url: string, options?: Parameters<typeof git.clone>[0]) {
+//     return git.clone({
+//       fs: this.fs,
+//       dir: this.dir,
+//       http,
+//       url,
+//       ...options,
+//     });
+//   }
+
+//   async status(filepath: string) {
+//     return git.status({
+//       fs: this.fs,
+//       dir: this.dir,
+//       filepath,
+//     });
+//   }
+
+//   async add(filepath: string) {
+//     return git.add({
+//       fs: this.fs,
+//       dir: this.dir,
+//       filepath,
+//     });
+//   }
+
+//   async commit(message: string, author: { name: string; email: string }) {
+//     return git.commit({
+//       fs: this.fs,
+//       dir: this.dir,
+//       message,
+//       author,
+//     });
+//   }
+
+//   async log(options?: Partial<Parameters<typeof git.log>[0]>) {
+//     return git.log({
+//       fs: this.fs,
+//       dir: this.dir,
+//       ...options,
+//     });
+//   }
+
+//   // Add more methods as needed...
+// }
