4c29f80cd498ea00a0f6f9b946669ae558b920e2
rbbydotdev <robby.polana+github@gmail.com>
2025-08-10 11:49:34 +0700

    ok it looks like it is working here,

--- src/components/Editor/InitializedMDXEditor.tsx
+++ src/components/Editor/InitializedMDXEditor.tsx
@@ -1,14 +1,14 @@
-"use client";
-// InitializedMDXEditor.tsx
-import { MDXEditor, type MDXEditorMethods, type MDXEditorProps } from "@mdxeditor/editor";
-import "@mdxeditor/editor/style.css";
-import { type ForwardedRef } from "react";
-import "./mdxeditor-custom-styles.css";
+// "use client";
+// // InitializedMDXEditor.tsx
+// import { MDXEditor, type MDXEditorMethods, type MDXEditorProps } from "@mdxeditor/editor";
+// import "@mdxeditor/editor/style.css";
+// import { type ForwardedRef } from "react";
+// import "./mdxeditor-custom-styles.css";
 
-// Only import this to the next file
-export default function InitializedMDXEditor({
-  editorRef,
-  ...props
-}: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) {
-  return <MDXEditor {...props} ref={editorRef} />;
-}
+// // Only import this to the next file
+// export default function InitializedMDXEditor({
+//   editorRef,
+//   ...props
+// }: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) {
+//   return <MDXEditor {...props} ref={editorRef} />;
+// }
