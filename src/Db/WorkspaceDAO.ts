6b964ab28b8807e620cfb120ce86de33d9757ff1
rbbydotdev <robby.polana+github@gmail.com>
2025-08-13 18:38:47 +0700

    coool

--- src/Db/WorkspaceDAO.ts
+++ src/Db/WorkspaceDAO.ts
@@ -1,6 +1,6 @@
 import { DiskJType, DiskType } from "@/Db/Disk";
 import { ClientDb } from "@/Db/instance";
-import { RemoteAuthDAO, RemoteAuthJTypePrivte } from "@/Db/RemoteAuth";
+import { RemoteAuthDAO, RemoteAuthJTypePrivate } from "@/Db/RemoteAuth";
 import { Workspace } from "@/Db/Workspace";
 import { WorkspaceRecord } from "@/Db/WorkspaceRecord";
 import { BadRequestError, errF, NotFoundError } from "@/lib/errors";
@@ -238,7 +238,7 @@ export class WorkspaceDAO {
     name: string;
     disk: DiskDAO | DiskJType;
     thumbs: DiskDAO | DiskJType;
-    remoteAuths: RemoteAuthJTypePrivte[];
+    remoteAuths: RemoteAuthJTypePrivate[];
   }) {
     this.guid = guid;
     this.name = name;
