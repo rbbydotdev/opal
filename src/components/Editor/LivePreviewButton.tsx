4bb15871fe3754ca9be8fe224382ee0e86ef5ba8
rbbydotdev <robby.polana+github@gmail.com>
2025-08-20 21:54:07 +0700

    implement live preview functionality in editor sidebar layout

--- src/components/Editor/LivePreviewButton.tsx
+++ src/components/Editor/LivePreviewButton.tsx
@@ -1,19 +1,30 @@
+import { getPreviewPaneElement } from "@/app/EditorSidebarLayout";
 import { useScrollSync } from "@/components/ScrollSync";
 import { Button } from "@/components/ui/button";
-import { Link } from "@tanstack/react-router";
 import { Zap } from "lucide-react";
 
 export function LivePreviewButton({ disabled }: { disabled?: boolean }) {
+  const handlePreviewPane = () => {
+    const previewPane = getPreviewPaneElement();
+    if (previewPane) {
+      console.log(previewPane);
+      previewPane.innerHTML = `<iframe src="${previewURL}" style="width:100%;height:100%;" />`;
+      // previewPane.classList.toggle("hidden");
+    }
+  };
   const { previewURL } = useScrollSync();
   return disabled ? (
     <Button size="sm" className="opacity-70" disabled={disabled}>
       Live Preview <Zap className="!text-primary-foreground" />
     </Button>
   ) : (
-    <Button size="sm" className="pointer-events-auto" asChild>
-      <Link to={previewURL} target="_blank">
-        Live Preview <Zap className="!text-primary-foreground" />
-      </Link>
+    <Button size="sm" className="pointer-events-auto" onClick={handlePreviewPane}>
+      Live Preview <Zap className="!text-primary-foreground" />
     </Button>
+    // <Button size="sm" className="pointer-events-auto" asChild>
+    //   <Link to={previewURL} target="_blank">
+    //     Live Preview <Zap className="!text-primary-foreground" />
+    //   </Link>
+    // </Button>
   );
 }
