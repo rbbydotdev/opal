57277fd058bfbcdfb1a30cf74b2003a194de300c
rbbydotdev <robby.polana+github@gmail.com>
2025-10-26 17:21:19 +0700

    argh!

--- src/components/ui/SelectableList2.tsx
+++ src/components/ui/SelectableList2.tsx
@@ -1,7 +1,5 @@
 // import { ConnectionsModal } from "@/components/connections-modal";
 import { useConfirm } from "@/components/Confirm";
-import { ConnectionsModal } from "@/components/ConnectionsModal";
-import { RemoteAuthSourceIconComponent } from "@/components/RemoteAuthSourceIcon";
 import { SidebarGripChevron } from "@/components/SidebarFileMenu/build-section/SidebarGripChevron";
 import { EmptySidebarLabel } from "@/components/SidebarFileMenu/EmptySidebarLabel";
 import { Button } from "@/components/ui/button";
@@ -22,17 +20,46 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { RemoteAuthJType, RemoteAuthRecord } from "@/Db/RemoteAuthTypes";
 import { useSingleItemExpander } from "@/features/tree-expander/useSingleItemExpander";
-import { useRemoteAuths } from "@/hooks/useRemoteAuths";
 import { IS_MAC } from "@/lib/isMac";
-import { cn } from "@/lib/utils";
-import { Check, Delete, Ellipsis, MoreHorizontal, Pencil, Plus, Sparkle, SquareDashed } from "lucide-react";
-import { useEffect, useRef, useState } from "react";
+import {
+  Check,
+  Delete,
+  Ellipsis,
+  Github,
+  LucideProps,
+  MoreHorizontal,
+  Pencil,
+  Sparkle,
+  SquareDashed,
+} from "lucide-react";
+import React, { useEffect, useRef, useState } from "react";
 
-function ConnectionManager() {
-  const { remoteAuths: remoteAuths, deleteRemoteAuth } = useRemoteAuths();
-  const [editingConnection, setEditingConnection] = useState<RemoteAuthJType | null>(null);
+export type ItemType = {
+  id: string;
+  label: React.ReactNode;
+  subLabel: React.ReactNode;
+  Icon: React.ComponentType<LucideProps>;
+};
+
+function ItemsSectionManager({
+  children,
+  setEditingId,
+  // editingId,
+}: {
+  children: React.ReactNode;
+  setEditingId: (id: string | null) => void;
+  // editingId: string | null;
+}) {
+  const items: ItemType[] = [
+    { id: "1", Icon: Github, label: "Connection 1", subLabel: "Details about connection 1" },
+    { id: "2", Icon: Github, label: "Connection 2", subLabel: "Details about connection 2" },
+  ]; // Placeholder for useRemoteAuthRecords();
+  // const handleSelect = (id: string) => {};
+
+  const deleteRemoteAuth = (id: string) => {
+    console.log("Delete connection with id:", id);
+  }; // Placeholder for useDeleteRemoteAuth();
   const [selected, setSelected] = useState<string[]>([]);
   const toggleSelected = (id: string) =>
     isSelected(id) ? setSelected((prev) => prev.filter((i) => i !== id)) : setSelected((prev) => [...prev, id]);
@@ -41,8 +68,8 @@ function ConnectionManager() {
 
   const { open: openConfirm } = useConfirm();
 
-  const handleEdit = (connection: RemoteAuthRecord) => {
-    setEditingConnection(connection);
+  const handleEdit = (id: string) => {
+    setEditingId(id);
   };
 
   useEffect(() => {
@@ -64,32 +91,23 @@ function ConnectionManager() {
       `Are you sure you want to delete ${selected.length} connection(s)? This action cannot be undone.`
     );
 
-  const handleSelect = (
-    sectionRef: React.RefObject<HTMLDivElement | null>,
-    event: React.MouseEvent,
-    connection: RemoteAuthRecord
-  ) => {
+  const handleSelect = (sectionRef: React.RefObject<HTMLDivElement | null>, event: React.MouseEvent, id: string) => {
     event.preventDefault();
 
     if (event.metaKey || event.ctrlKey) {
       if (sectionRef?.current) sectionRef.current.focus();
-      toggleSelected(connection.guid);
+      toggleSelected(id);
     } else {
-      handleEdit(connection);
+      handleEdit(id);
     }
   };
 
   return (
     <>
-      <div>
-        <ConnectionsModal>
-          <SidebarGroupAction className={cn("top-1.5 p-0", { "right-8": remoteAuths.length })} title="Add Connection">
-            <Plus className="w-4 h-4" /> <span className="sr-only">Add Connection</span>
-          </SidebarGroupAction>
-        </ConnectionsModal>
+      <div className="group-data-[state=closed]/collapsible:hidden">
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            {remoteAuths.length && (
+            {items.length && (
               <SidebarGroupAction className="top-1.5 p-0 right-0" title="Connections Menu">
                 <Ellipsis /> <span className="sr-only">Connections Menu</span>
               </SidebarGroupAction>
@@ -97,9 +115,9 @@ function ConnectionManager() {
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
             <DropdownMenuItem
-              onClick={() => setSelected(remoteAuths.map((r) => r.guid))}
+              onClick={() => setSelected(items.map((r) => r.id))}
               className="grid grid-cols-[auto_1fr] items-center gap-2"
-              disabled={remoteAuths.length === selected.length}
+              disabled={items.length === selected.length}
             >
               <Check className="w-4 h-4" />
               <span>Select All</span>
@@ -129,46 +147,30 @@ function ConnectionManager() {
         </DropdownMenu>
       </div>
 
-      <ConnectionsModal
-        mode="edit"
-        editConnection={editingConnection!}
-        open={Boolean(editingConnection)}
-        onOpenChange={(open) => {
-          if (!open) setEditingConnection(null);
-        }}
-        onSuccess={() => setEditingConnection(null)}
-      />
+      {children}
 
       <CollapsibleContent>
         <SidebarMenu>
-          {remoteAuths.length === 0 && (
+          {items.length === 0 && (
             <div className="px-4 py-2">
               <EmptySidebarLabel label="no connections" />
             </div>
           )}
-          {remoteAuths.map((connection) => (
-            <SidebarMenuItem key={connection.guid}>
+          {items.map(({ Icon, subLabel, label, id }) => (
+            <SidebarMenuItem key={id}>
               <div className="group flex items-center pr-1 py-0">
-                <SidebarMenuButton
-                  className="flex-1 min-w-0 pl-4"
-                  onClick={(e) => handleSelect(sectionRef, e, connection)}
-                >
+                <SidebarMenuButton className="flex-1 min-w-0 pl-4" onClick={(e) => handleSelect(sectionRef, e, id)}>
                   <div className="flex items-center flex-1 min-w-0 gap-1 text-xs ml-[0.17rem]">
                     <div className="w-4 h-4 flex justify-center items-center mr-0.5 shrink-0">
-                      {isSelected(connection.guid) && <Check className="w-3 h-3 rounded-full _border" />}
+                      {isSelected(id) && <Check className="w-3 h-3 rounded-full _border" />}
                     </div>
-                    <RemoteAuthSourceIconComponent
-                      type={connection.type}
-                      source={connection.source}
-                      size={12}
-                      className="flex-shrink-0"
-                    />
+                    <Icon size={12} className="flex-shrink-0" />
                     <span className="max-w-[32ch] font-mono text-xs overflow-hidden text-ellipsis whitespace-nowrap flex-shrink ml-1">
-                      {connection.name}
+                      {label}
                     </span>
                     <span className="flex-shrink-0 text-xs">{"/"}</span>
                     <span className="text-xs text-muted-foreground capitalize font-mono overflow-hidden text-ellipsis whitespace-nowrap">
-                      {`${connection.type} ${connection.source}`.toLowerCase()}
+                      {subLabel}
                     </span>
                   </div>
                 </SidebarMenuButton>
@@ -183,11 +185,11 @@ function ConnectionManager() {
                     </Button>
                   </DropdownMenuTrigger>
                   <DropdownMenuContent align="end">
-                    <DropdownMenuItem onClick={() => handleEdit(connection)}>
+                    <DropdownMenuItem onClick={() => handleEdit(id)}>
                       <Pencil className="w-4 h-4 mr-2" />
                       Edit
                     </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => deleteRemoteAuth(connection.guid)}>
+                    <DropdownMenuItem onClick={() => deleteRemoteAuth(id)}>
                       <Delete className="w-4 h-4 mr-2 text-destructive" />
                       Delete
                     </DropdownMenuItem>
@@ -202,30 +204,42 @@ function ConnectionManager() {
   );
 }
 
-export function SidebarConnectionsSection(props: React.ComponentProps<typeof SidebarGroup>) {
+function ItemsSection({ children, ...props }: React.ComponentProps<typeof SidebarGroup>) {
   const [expanded, setExpand] = useSingleItemExpander("connections");
 
   return (
     <SidebarGroup className="pl-0 py-0" {...props}>
       <Collapsible className="group/collapsible flex flex-col min-h-0" open={expanded} onOpenChange={setExpand}>
-        <CollapsibleTrigger asChild>
-          <SidebarMenuButton className="pl-0">
-            <SidebarGroupLabel className="pl-2">
-              <SidebarGripChevron />
-              <div className="w-full">
-                <div className="flex justify-center items-center">
-                  <Sparkle size={12} className="mr-2" />
-                  Connections
-                </div>
-              </div>
-            </SidebarGroupLabel>
-          </SidebarMenuButton>
-        </CollapsibleTrigger>
-
-        <div className="group-data-[state=closed]/collapsible:hidden">
-          <ConnectionManager />
-        </div>
+        {children}
       </Collapsible>
     </SidebarGroup>
+  );
+}
+// <ItemsSectionHeader />
+// <ItemsSectionManager setEditingId={() => {}} children={null} />
+function Foo() {
+  return (
+    <ItemsSection>
+      <ItemsSectionHeader>
+        <Sparkle size={12} className="mr-2" />
+        Connections
+      </ItemsSectionHeader>
+      <ItemsSectionManager setEditingId={() => {}} children={null} />
+    </ItemsSection>
+  );
+}
+
+function ItemsSectionHeader({ children }: { children: React.ReactNode }) {
+  return (
+    <CollapsibleTrigger asChild>
+      <SidebarMenuButton className="pl-0">
+        <SidebarGroupLabel className="pl-2">
+          <SidebarGripChevron />
+          <div className="w-full">
+            <div className="w-full flex justify-center items-center">{children}</div>
+          </div>
+        </SidebarGroupLabel>
+      </SidebarMenuButton>
+    </CollapsibleTrigger>
   );
 }
