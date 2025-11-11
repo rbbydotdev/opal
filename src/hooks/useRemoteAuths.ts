805169b6b8a30549d78386032e171f041cf9c99b
rbbydotdev <robby.polana+github@gmail.com>
2025-11-11 19:30:24 +0700

    refactor: simplify remote auth management in PublicationModal and SidebarConnectionsSections

--- src/hooks/useRemoteAuths.ts
+++ src/hooks/useRemoteAuths.ts
@@ -1,15 +1,10 @@
 import { ClientDb } from "@/data/instance";
-import { RemoteAuthDAO } from "@/data/RemoteAuth";
 import { RemoteAuthRecord } from "@/data/RemoteAuthTypes";
 import { useLiveQuery } from "dexie-react-hooks";
 
 export function useRemoteAuths() {
   const remoteAuths = useLiveQuery(() => ClientDb.remoteAuths.toArray(), [], []) as RemoteAuthRecord[];
-  const deleteRemoteAuth = async (id: string) => {
-    await RemoteAuthDAO.deleteByGuid(id);
-  };
   return {
     remoteAuths,
-    deleteRemoteAuth,
   };
 }
