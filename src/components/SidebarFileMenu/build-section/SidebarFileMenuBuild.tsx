3b8a064a7e8f826dda71a5578ea232d1fefede74
rbbydotdev <robby.polana+github@gmail.com>
2025-11-15 13:49:22 +0700

    refactor publication modal components and update destination logic

--- src/components/SidebarFileMenu/build-section/SidebarFileMenuBuild.tsx
+++ src/components/SidebarFileMenu/build-section/SidebarFileMenuBuild.tsx
@@ -1,7 +1,7 @@
 import { BuildModal, useBuildModalCmd } from "@/components/BuildModal";
 import { useConfirm } from "@/components/Confirm";
 import { FileTreeMenuCtxProvider } from "@/components/FileTreeMenuCtxProvider";
-import { PublicationModal, usePublicationModalCmd } from "@/components/PublicationModal";
+import { PublicationModal, usePublicationModalCmd } from "@/components/publication-modal/PublicationModal";
 import { BuildSelector } from "@/components/SidebarFileMenu/build-files-section/BuildSelector";
 import { BuildSidebarFileMenuFileSection } from "@/components/SidebarFileMenu/build-files-section/BuildSidebarFileMenuFileSection";
 import { useBuildManager } from "@/components/SidebarFileMenu/build-files-section/useBuildManager";
