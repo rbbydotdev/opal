c73318aad2d6ec6d0ea2b7e0b6f3927fb65b0a0a
rbbydotdev <robby.polana+github@gmail.com>
2025-11-24 00:19:12 +0700

    move context inside

--- src/data/NullDisk.ts
+++ src/data/NullDisk.ts
@@ -1,8 +1,35 @@
 import { Disk } from "@/data/disk/Disk";
+import { DiskContext } from "@/data/disk/DiskContext";
 import { DiskDAO } from "@/data/disk/DiskDAO";
-import { NullDiskContext } from "@/data/disk/NullDiskContext";
 import { DiskType } from "@/data/DiskType";
+import { CommonFileSystem, NullFileSystem } from "@/data/FileSystemTypes";
+import { FileTree } from "@/lib/FileTree/Filetree";
 import { TreeDirRootJType } from "@/lib/FileTree/TreeNode";
+import { Mutex } from "async-mutex";
+
+export class NullDiskContext extends DiskContext {
+  constructor(
+    fs: CommonFileSystem,
+    fileTree: FileTree,
+    mutex: Mutex
+  ) {
+    super(fs, fileTree, mutex);
+  }
+
+  static create(
+    guid: string = "__disk__NullDisk"
+  ): NullDiskContext {
+    const fs = NullFileSystem;
+    const mutex = new Mutex();
+    const fileTree = new FileTree(fs, guid, mutex);
+
+    return new NullDiskContext(fs, fileTree, mutex);
+  }
+
+  async tearDown(): Promise<void> {
+    // NullFileSystem doesn't require cleanup
+  }
+}
 
 export class NullDisk extends Disk<NullDiskContext> {
   static type: DiskType = "NullDisk";
