b47a746327b578dd0ec3e2afca881629971a1c86
rbbydotdev <robby.polana+github@gmail.com>
2025-12-01 19:11:26 +0700

    sidebar focus styles and implement double Cmd focus hook

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
+        xs: "",
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs [&_svg]:size-3",
         lg: "h-10 rounded-md px-8",
