51e0e1de81ff609a3dabe3dc873bfb1e85fe2105
rbbydotdev <robby.polana+github@gmail.com>
2025-12-03 13:59:58 +0700

    refactor: remove unused route components from sandbox and all-settings

--- src/routes/all-settings/$.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/all-settings/$")({
-  component: SettingsPage,
-});
-
-function SettingsPage() {
-  return <h1>Hello "/settings/$"!</h1>;
-}
