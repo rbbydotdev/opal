94ceb6e1096aab7990ee163e6e1d941542f2a3a1
rbbydotdev <robby.polana+github@gmail.com>
2025-12-03 15:01:40 +0700

    ok

--- src/components/sidebar/SidebarMenuSections.tsx
+++ src/components/sidebar/SidebarMenuSections.tsx
@@ -147,7 +147,7 @@ export function SidebarMenuSections({ ...props }: React.ComponentProps<typeof Si
           </DropdownMenuTrigger>
         </SidebarGroupLabel>
       </DropdownMenu>
-      <div ref={sidebarListRef} className="overflow-y-auto no-scrollbar pr-4" tabIndex={-1}>
+      <div ref={sidebarListRef} className="sidebar-menu-section-list overflow-y-auto no-scrollbar pr-4" tabIndex={-1}>
         <SidebarMenuDndList show={storedValue} currentWorkspace={currentWorkspace} />
       </div>
     </SidebarGroup>
