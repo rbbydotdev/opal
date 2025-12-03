0af02e77b24222ac206e87596b15f9ba7e92890e
rbbydotdev <robby.polana+github@gmail.com>
2025-12-03 12:44:59 +0700

    stash

--- src/builder/builder-types.ts
+++ src/builder/builder-types.ts
@@ -1,17 +1,5 @@
-import { BuildStrategy } from "@/data/DAO/records/BuildRecord";
-import { Disk } from "@/data/disk/Disk";
 import { TreeNode } from "@/lib/FileTree/TreeNode";
-import { AbsPath, RelPath } from "@/lib/paths2";
-
-interface BuildOptions {
-  strategy: BuildStrategy;
-  sourceDisk: Disk;
-  outputDisk: Disk;
-  sourcePath: AbsPath;
-  outputPath: AbsPath;
-  onLog?: (message: string) => void;
-  onError?: (error: string) => void;
-}
+import { RelPath } from "@/lib/paths2";
 
 interface FrontMatter {
   layout?: string;
