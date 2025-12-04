cdd64da2b7b6d81c47a03c8e54bd8ff37f8b4539
rbbydotdev <robby.polana+github@gmail.com>
2025-12-04 11:22:47 +0700

    stash

--- src/components/isParent.tsx
+++
@@ -1,6 +0,0 @@
-import mdast from "mdast";
-import unist from "unist";
-
-export function isParent(node: unknown): node is unist.Parent {
-  return Boolean(typeof (node as mdast.Parent).children !== "undefined");
-}
