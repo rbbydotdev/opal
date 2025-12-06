6d381cd49965e384aca0ce9865cde30277c1d739
rbbydotdev <robby.polana+github@gmail.com>
2025-12-06 12:08:54 +0700

    refactor: move RemoteResource context and types to separate file

---
+++ src/components/publish-modal/RemoteResourceMode.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, useContext } from "react";
+import { Control, FieldPath, FieldValues } from "react-hook-form";
+
+export const RemoteResourceContext = createContext<RemoteResourceContextValue<any, any> | null>(null);
+export function useRemoteResourceContext<T extends FieldValues, K extends FieldPath<T>>() {
+  const context = useContext(RemoteResourceContext);
+  if (!context) {
+    throw new Error("RemoteResource compound components must be used within RemoteResourceRoot");
+  }
+  return context as RemoteResourceContextValue<T, K>;
+}
+export type RemoteResourceMode = "search" | "input" | "create" | "input/success";
+export type RemoteResourceContextValue<T extends FieldValues, K extends FieldPath<T>> = {
+  mode: RemoteResourceMode;
+  setMode: (mode: RemoteResourceMode) => void;
+  control: Control<T>;
+  fieldName: K;
+  onValueChange: (value: string) => void;
+  getValue: () => string | undefined;
+  inputRef: React.RefObject<HTMLInputElement | null>;
+};
